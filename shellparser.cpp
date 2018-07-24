60494eac10d47f0b0fc77b9447d9327ba205abe0
Wonseok Seo <wonseokseo@Wonseoks-MacBook-Pro.local>
2018-07-24 11:56:05 -0700

    updated

--- shellparser.cpp
+++ shellparser.cpp
@@ -27,53 +27,13 @@ DbEnv* _DB_ENV;
 class DBParser{
 
 public:
-// static const vector<string> reserved_words;
-// reserved_words = {
-// "COLUMNS", "SHOW", "TABLES",
-// "ADD","ALL","ALLOCATE","ALTER","AND","ANY","ARE","ARRAY","AS","ASENSITIVE","ASYMMETRIC","AT",
-//                   "ATOMIC","AUTHORIZATION","BEGIN","BETWEEN","BIGINT","BINARY","BLOB","BOOLEAN","BOTH","BY","CALL",
-//                   "CALLED","CASCADED","CASE","CAST","CHAR","CHARACTER","CHECK","CLOB","CLOSE","COLLATE","COLUMN",
-//                   "COMMIT","CONNECT","CONSTRAINT","CONTINUE","CORRESPONDING","CREATE","CROSS","CUBE","CURRENT",
-//                   "CURRENT_DATE","CURRENT_DEFAULT_TRANSFORM_GROUP","CURRENT_PATH","CURRENT_ROLE","CURRENT_TIME",
-//                   "CURRENT_TIMESTAMP","CURRENT_TRANSFORM_GROUP_FOR_TYPE","CURRENT_USER","CURSOR","CYCLE","DATE",
-//                   "DAY","DEALLOCATE","DEC","DECIMAL","DECLARE","DEFAULT","DELETE","DEREF","DESCRIBE","DETERMINISTIC",
-//                   "DISCONNECT","DISTINCT","DOUBLE","DROP","DYNAMIC","EACH","ELEMENT","ELSE","END","END-EXEC","ESCAPE",
-//                   "EXCEPT","EXEC","EXECUTE","EXISTS","EXTERNAL","FALSE","FETCH","FILTER","FLOAT","FOR","FOREIGN",
-//                   "FREE","FROM","FULL","FUNCTION","GET","GLOBAL","GRANT","GROUP","GROUPING","HAVING","HOLD","HOUR",
-//                   "IDENTITY","IMMEDIATE","IN","INDICATOR","INNER","INOUT","INPUT","INSENSITIVE","INSERT","INT",
-//                   "INTEGER","INTERSECT","INTERVAL","INTO","IS","ISOLATION","JOIN","LANGUAGE","LARGE","LATERAL",
-//                   "LEADING","LEFT","LIKE","LOCAL","LOCALTIME","LOCALTIMESTAMP","MATCH","MEMBER","MERGE","METHOD",
-//                   "MINUTE","MODIFIES","MODULE","MONTH","MULTISET","NATIONAL","NATURAL","NCHAR","NCLOB","NEW","NO",
-//                   "NONE","NOT","NULL","NUMERIC","OF","OLD","ON","ONLY","OPEN","OR","ORDER","OUT","OUTER","OUTPUT",
-//                   "OVER","OVERLAPS","PARAMETER","PARTITION","PRECISION","PREPARE","PRIMARY","PROCEDURE","RANGE",
-//                   "READS","REAL","RECURSIVE","REF","REFERENCES","REFERENCING","REGR_AVGX","REGR_AVGY","REGR_COUNT",
-//                   "REGR_INTERCEPT","REGR_R2","REGR_SLOPE","REGR_SXX","REGR_SXY","REGR_SYY","RELEASE","RESULT","RETURN",
-//                   "RETURNS","REVOKE","RIGHT","ROLLBACK","ROLLUP","ROW","ROWS","SAVEPOINT","SCROLL","SEARCH","SECOND",
-//                   "SELECT","SENSITIVE","SESSION_USER","SET","SIMILAR","SMALLINT","SOME","SPECIFIC","SPECIFICTYPE",
-//                   "SQL","SQLEXCEPTION","SQLSTATE","SQLWARNING","START","STATIC","SUBMULTISET","SYMMETRIC","SYSTEM",
-//                   "SYSTEM_USER","TABLE","THEN","TIME","TIMESTAMP","TIMEZONE_HOUR","TIMEZONE_MINUTE","TO","TRAILING",
-//                   "TRANSLATION","TREAT","TRIGGER","TRUE","UESCAPE","UNION","UNIQUE","UNKNOWN","UNNEST","UPDATE",
-//                   "UPPER","USER","USING","VALUE","VALUES","VAR_POP","VAR_SAMP","VARCHAR","VARYING","WHEN","WHENEVER",
-//                   "WHERE","WIDTH_BUCKET","WINDOW","WITH","WITHIN","WITHOUT","YEAR"};
-//
-//   /**
-// 	 * 	check existence of candiate word
-// 	 * 	@param candiate 	target string word to check
-// 	 *	@return 					if it exsits return true, if not return false
-// 	 */
-// 	static bool is_reserved_word(string candidate) {
-// 			for (auto const& word: reserved_words)
-// 					if (candidate == word)
-// 							return true;
-// 			return false;
-// 	}
 
 	/**
 	* Parse TABLE REF INFO
 	* @param	TableRef *table
 	* @return	string
 	*/
-	static string printTableRefInfo(TableRef *table){
+	static string printTableRefInfo(const TableRef *table){
 		string tableref;
 		switch (table->type){
 		case kTableSelect:
@@ -126,7 +86,7 @@ public:
 	* @param	Expr *expr
 	* @return	string
 	*/
-	static string printOperatorExpression(Expr *expr){
+	static string printOperatorExpression(const Expr *expr){
 		string operatorExpression;
 		if (expr == nullptr){
 			operatorExpression = "null";
@@ -161,7 +121,7 @@ public:
 	* @param	Expr *expr
 	* @return	string
 	*/
-	static string printExpression(Expr *expr){
+	static string printExpression(const Expr *expr){
 		string expression;
 		switch (expr->type){
 		case kExprStar:
@@ -371,7 +331,7 @@ int main(int argc, char *argv[]) {
 		cout << "(sql5300: running with database environment at " << envHome << ")" << endl;
 		DBParser dbParser;
 		// Initialize dbenv
-		DbEnv env(0U);
+		DbEnv *env = new DbEnv(0U);
 		env.set_message_stream(&cout);
 		env.set_error_stream(&cerr);
 		try {
@@ -380,7 +340,7 @@ int main(int argc, char *argv[]) {
 				cerr << "(sql5300: " << exc.what() << ")" << endl;
 				exit(1);
 		}
-		_DB_ENV = &env;
+		_DB_ENV = env;
 		initialize_schema_tables();
 		//Db db(&env, 0);
 		while(true){
